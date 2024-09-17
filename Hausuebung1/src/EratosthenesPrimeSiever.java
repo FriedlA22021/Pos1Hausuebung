5a80885ac756e7cdb87f67b0e68b83e4dadace9c
FriedlA22021 <FriedlA22021@sus.htl-grieskirchen.at>
2024-09-17 11:54:36 +0200

    printPrime - Methode implementiert
    exit
    exit
    s

--- Hausuebung1/src/EratosthenesPrimeSiever.java
+++ Hausuebung1/src/EratosthenesPrimeSiever.java
@@ -1,4 +1,15 @@
-public class EratosthenesPrimeSiever implements PrimeSiever{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EratosthenesPrimeSiever implements PrimeSiever {
+    private int upperLimit;
+
+    public EratosthenesPrimeSiever(int upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+
     @Override
     public boolean isPrime(int p) {
         if (p <= 1) {
@@ -20,6 +31,10 @@ public class EratosthenesPrimeSiever implements PrimeSiever{
 
     @Override
     public void printPrimes() {
-
+        for (int i = 0; i < upperLimit; i++) {
+            if (isPrime(i) == true) {
+                System.out.println(i  + " ist eine Primzahl");
+            }
+        }
     }
 }
