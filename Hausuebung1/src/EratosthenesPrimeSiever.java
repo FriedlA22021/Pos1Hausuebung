90676bb9276429d5d30b742b074c395ada5a3019
FriedlA22021 <FriedlA22021@sus.htl-grieskirchen.at>
2024-09-17 11:31:27 +0200

    Klasse EratosthenesPrimeSiever erstellt und Interface Klasse implementiert

---
+++ Hausuebung1/src/EratosthenesPrimeSiever.java
@@ -0,0 +1,11 @@
+public class EratosthenesPrimeSiever implements PrimeSiever{
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
