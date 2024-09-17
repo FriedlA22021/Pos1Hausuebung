fe2964399f7b2d9ab627f1cf9d5ac12d11dc9bf9
FriedlA22021 <FriedlA22021@sus.htl-grieskirchen.at>
2024-09-17 11:34:55 +0200

    IsPrime Methode implementiert

--- Hausuebung1/src/EratosthenesPrimeSiever.java
+++ Hausuebung1/src/EratosthenesPrimeSiever.java
@@ -1,7 +1,21 @@
 public class EratosthenesPrimeSiever implements PrimeSiever{
     @Override
     public boolean isPrime(int p) {
-        return false;
+        if (p <= 1) {
+            return false;
+        }
+        if (p == 2 || p == 3) {
+            return true;
+        }
+        if (p % 2 == 0 || p % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= p; i += 6) {
+            if (p % i == 0 || p % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
